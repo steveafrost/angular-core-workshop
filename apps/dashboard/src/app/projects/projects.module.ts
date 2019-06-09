481ac91159a68d6680426f0a6d9083794f324466
Steve Frost <sfrost@nerdery.com>
2019-06-09 16:57:54 -0500

    lesson 9

--- apps/dashboard/src/app/projects/projects.module.ts
+++ apps/dashboard/src/app/projects/projects.module.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectsComponent } from './projects.component';
+import { MaterialModule } from '@workshop/material';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    ProjectsRoutingModule
-  ],
-  declarations: [ProjectsComponent]
+  imports: [CommonModule, ProjectsRoutingModule, MaterialModule],
+  declarations: [ProjectsComponent],
+  exports: [ProjectsComponent]
 })
-export class ProjectsModule { }
+export class ProjectsModule {}
